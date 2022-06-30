f6771be7205305d186edcaef34b0466a1aeeae00
MarioTavarezCK <mario.tavarez@clikalia.com>
2022-06-29 21:01:23 -0500

    Index.js modified

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Mario</a>
+          Welcome to <a href="https://nextjs.org">Mario Tavarez</a>
         </h1>
 
         <p className={styles.description}>
